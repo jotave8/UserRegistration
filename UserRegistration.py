dc236bbc0e2b6ed0243e70f6622e37f0cc1bd72a
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-05-22 14:07:32 +0000

    finished listar_aluno

--- UserRegistration.py
+++ UserRegistration.py
@@ -5,9 +5,7 @@ sair = 0
 
 lista_usuarios = [None] * TAM_USUARIO
 
-
 def menu_geral():
-
     print('╔═════════════════════════════════════════╗');
     print('║           Cadastro de Usuário           ║');
     print('║═════════════════════════════════════════║');
@@ -20,17 +18,27 @@ def menu_geral():
     return int(input('Escolha: '))
 
 def cadastrar_usuario(lista_usuarios, qtd_usuario):
-    print('Cadastrar Usuario')
-    usuario = {
-        "nome": input('Informe o nome: ').upper(),
-        "idade": int(input('Informe a idade: ')),
-        "email": input('Infome o email: ').upper()
-    }
+    print('\nCadastrar Usuario')
+    if qtd_usuario == TAM_USUARIO:
+        print('Lista de usuarios cheia')
+    else:
+        usuario = {
+            "nome": input('Informe o nome: ').upper(),
+            "idade": int(input('Informe a idade: ')),
+            "email": input('Infome o email: ').upper()
+        }
+        lista_usuarios[qtd_usuario] = usuario
+        qtd_usuario += 1
+        print('Usuário cadastrado com sucesso!\n')
+    return qtd_usuario
 
-    lista_usuarios[qtd_usuario] = usuario
-    qtd_usuario += 1
-    print('Usuário cadastrado com sucesso!')
-
+def listar_usuarios (lista_usuarios, qtd_usuario):
+    print('\nListar Usuario')
+    if qtd_usuario == 0:
+        print('Lista de usuarios vazia\n')
+    else:
+        for i in range(qtd_usuario):
+            print(f'Nome: {lista_usuarios[i]["nome"]}  | Idade: {lista_usuarios[i]["idade"]}  | Email: {lista_usuarios[i]["email"]}')
 
 while sair != 1:
     opcao = menu_geral()
@@ -39,4 +47,6 @@ while sair != 1:
         case 0 :
             sair = 1
         case 1 :
-            sair = 0
+            qtd_usuario = cadastrar_usuario(lista_usuarios, qtd_usuario)
+        case 2 :
+            listar_usuarios(lista_usuarios, qtd_usuario)
